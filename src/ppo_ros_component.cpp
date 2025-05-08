e874dcd275e5ff69840b4bc1f185cda6f33f7f20
Masaya Kataoka <ms.kataoka@gmail.com>
2025-05-08 23:01:01 +0900

    fix linker option
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- src/ppo_ros_component.cpp
+++ src/ppo_ros_component.cpp
@@ -20,18 +20,23 @@ PPOComponent::PPOComponent(const rclcpp::NodeOptions & options)
 : Node("ppo_component", options),
   parameters_(ppo_ros::ParamListener(get_node_parameters_interface()).get_params())
 {
-  actor = torch::jit::load(
-    ament_index_cpp::get_package_share_directory(parameters_.model_package) + "/" +
-    parameters_.model);
   RCLCPP_INFO_STREAM(
     get_logger(), "Loading model from "
                     << ament_index_cpp::get_package_share_directory(parameters_.model_package) +
                          "/" + parameters_.model);
-  action_publisher_ = this->create_publisher<AdaptedType>("action", 1);
-  observation_subscriber_ =
-    this->create_subscription<AdaptedType>("observation", 1, [this](const torch::Tensor & msg) {
-      action_publisher_->publish(actor.forward({msg}).toTensor());
-    });
+  actor = torch::jit::load(
+    ament_index_cpp::get_package_share_directory(parameters_.model_package) + "/" +
+    parameters_.model);
+  if(parameters_.use_cuda) {
+    actor.to(torch::kCUDA);
+  } else {
+    actor.to(torch::kCPU);
+  }
+  // action_publisher_ = this->create_publisher<AdaptedType>("action", 1);
+  // observation_subscriber_ =
+  //   this->create_subscription<AdaptedType>("observation", 1, [this](const torch::Tensor & msg) {
+  //     action_publisher_->publish(actor.forward({msg}).toTensor());
+  //   });
 }
 }  // namespace ppo_ros
 
